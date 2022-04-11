56d432372022f46ea4f320bd0316ef330b5eac62
Bread <primebox21@gmail.com>
2022-04-11 21:15:33 +0700

    clear extra

--- metaclasses.py
+++ metaclasses.py
@@ -5,9 +5,6 @@ from pprint import pprint
 class ServerVerifier(type):
     def __init__(cls, clsname, bases, clsdict):
         load_global = []
-        load_method = []
-        load_attr = []
-        # pprint(clsdict)
         for func in clsdict:
             try:
                 ret = dis.get_instructions(clsdict[func])
@@ -15,24 +12,10 @@ class ServerVerifier(type):
                 pass
             else:
                 for i in ret:
-                    # print(i)
                     if i.opname == 'LOAD_GLOBAL':
                         if i.argval not in load_global:
                             load_global.append(i.argval)
-                    elif i.opname == 'LOAD_METHOD':
-                        if i.argval not in load_method:
-                            load_method.append(i.argval)
-                    elif i.opname == 'LOAD_ATTR':
-                        if i.argval not in load_attr:
-                            load_attr.append(i.argval)
 
-        print(20 * '-', 'load_global', 20 * '-')
-        pprint(load_global)
-        print(20 * '-', 'load_method', 20 * '-')
-        pprint(load_method)
-        print(20 * '-', 'load_attr', 20 * '-')
-        pprint(load_attr)
-        print(50 * '-')
         if 'connect' in load_global:
             raise TypeError('Использование метода connect недопустимо в классе Server')
         if not ('SOCK_STREAM' in load_global and 'AF_INET' in load_global):
@@ -42,12 +25,7 @@ class ServerVerifier(type):
 
 class ClientVerifier(type):
     def __init__(cls, clsname, bases, clsdict):
-
         load_global = []
-        load_method = []
-        load_attr = []
-        # pprint(clsdict)
-
         for func in clsdict:
             try:
                 ret = dis.get_instructions(clsdict[func])
@@ -55,24 +33,9 @@ class ClientVerifier(type):
                 pass
             else:
                 for i in ret:
-                    # print(i)
                     if i.opname == 'LOAD_GLOBAL':
                         if i.argval not in load_global:
                             load_global.append(i.argval)
-                    elif i.opname == 'LOAD_METHOD':
-                        if i.argval not in load_method:
-                            load_method.append(i.argval)
-                    elif i.opname == 'LOAD_ATTR':
-                        if i.argval not in load_attr:
-                            load_attr.append(i.argval)
-
-        print(20 * '-', 'load_global', 20 * '-')
-        pprint(load_global)
-        print(20 * '-', 'load_method', 20 * '-')
-        pprint(load_method)
-        print(20 * '-', 'load_attr', 20 * '-')
-        pprint(load_attr)
-        print(50 * '-')
 
         if 'accept' in load_global and 'listen' in load_global:
             raise TypeError(f'В классе {clsname} обнаружено использование запрещённого приёма')
