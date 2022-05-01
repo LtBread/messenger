097eb986fd36f35e14f2fba3e741a394f81abf7c
Bread <primebox21@gmail.com>
2022-05-01 12:09:55 +0700

    updated common.utils.py, common.errors.py

--- common/utils.py
+++ common/utils.py
@@ -2,32 +2,34 @@ import json
 
 from common.errors import IncorrectDataRecivedError, NonDictInputError
 from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+from common.decorators import log
 
 
 # sys.path.append('../')
 
 
-# @log
+@log
 def get_message(client):
     """
     Получает сообщение в виде байтов и возвращает словарь,
     если получено что-то другое, поднимает ошибку значения
     """
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode(ENCODING)
-        response = json.loads(json_response)
-        if isinstance(response, dict):
-            return response
-        raise NonDictInputError
-    raise IncorrectDataRecivedError
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
+    # raise NonDictInputError
+    # raise IncorrectDataRecivedError
 
 
-# @log
+@log
 def send_message(sock, message):
     """ Принимает словарь, кодирует и отправляет сообщение в виде байтов """
-    if not isinstance(message, dict):
-        raise NonDictInputError
+    # if not isinstance(message, dict):
+    #     raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
