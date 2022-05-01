3aba8bbf8112a28adc4bc411dbd5e5f295116b70
Bread <primebox21@gmail.com>
2022-05-01 12:02:39 +0700

    updated launcher.py

--- launcher.py
+++ launcher.py
@@ -2,23 +2,35 @@
 import subprocess
 
 
-PROCESS = []
-NUM_CLIENTS = 2
+def main():
+    process = []
+    while True:
+        action = input('Выберете действие: q - выход, '
+                       's - запустить сервер и клиенты, '
+                       'l - запустить клиенты, '
+                       'x - закрыть все окна: ')
 
-while True:
-    ACTION = input('Выберете действие: q - выход, '
-                   's - запустить сервер и клиенты,'
-                   ' x - закрыть все окна: ')
+        if action == 'q':
+            break
+        elif action == 's':
+            process.append(
+                subprocess.Popen(
+                    'python server.py',
+                    creationflags=subprocess.CREATE_NEW_CONSOLE))
+        elif action == 'k':
+            print('Убедитесь, что на сервере зарегистрировано необходимое количество клиентов с паролем 123456')
+            clients_count = int(input('Введите количество тестовых клиентов: '))
+            # создание клиентов
+            for client in range(clients_count):
+                process.append(
+                    subprocess.Popen(
+                        f'python client.py -n test{client + 1} -p 123456',
+                        creationflags=subprocess.CREATE_NEW_CONSOLE))
 
-    if ACTION == 'q':
-        break
-    elif ACTION == 's':
-        PROCESS.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        elif action == 'x':
+            while process:
+                process.pop().kill()
 
-        # создание клиентов
-        for client in range(1, NUM_CLIENTS + 1):
-            PROCESS.append(subprocess.Popen('python __init__.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
 
-    elif ACTION == 'x':
-        while PROCESS:
-            PROCESS.pop().kill()
+if __name__ == '__main__':
+    main()
